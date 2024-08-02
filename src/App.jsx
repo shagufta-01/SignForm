59a48ab1c06e7b74f7a9cc1d116190ec976e1b0b
Shagufta fatima <129382422+shagufta-01@users.noreply.github.com>
2024-08-02 14:30:08 +0530

    feat/signUp

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginSignup from './Components/LoginSign/LoginSignup'
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  )
+}
+
+export default App
